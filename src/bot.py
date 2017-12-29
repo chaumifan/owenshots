73364d9757807766d650a643e1b949c9f4de82f4
Kevin <kevinchau@utexas.edu>
2017-12-29 03:58:07 -0600

    Working on image upload and command creation

--- src/bot.py
+++ src/bot.py
@@ -1,8 +1,16 @@
-import asyncio
-import discord
+import random
 from discord.ext import commands
 
+
 bot = commands.Bot(command_prefix="!")
+
+def create_dict():
+    img_dict = {}
+    with open("image.info", "r") as f:
+        for line in f:
+            (key, val) = line.split()
+            img_dict[key] = val
+    return img_dict
 
 @bot.event
 async def on_ready():
@@ -11,11 +19,46 @@ async def on_ready():
     print(bot.user.id)
     print("------")
 
+@bot.event
+async def on_message(message):
+    if message.content == ("!DIY"):
+        await client.send_file(channel,"my_image.png")
+#    if message.content in img_dict:
+#        await bot.say("found entry")
+    if message.content == ("!8ballchau"):
+        await bot.say("8 ball summoned")
+
 @bot.command()
-async def test(ctx, arg):
-    if arg == "help":
-        await ctx.send("List of commands: ")
-    if arg == "8ballchau":
-        await ctx.send("do your homework")
-    else:
-        await ctx.send(arg)
+async def repeat(times : int, content="repeating..."):
+    for i in range(times):
+        await bot.say(content)
+
+@bot.command()
+async def eightballchau():
+    await bot.say(random.choice(phrase))
+
+@bot.command(description="Description of test")
+async def echo(*args):
+    """Testing function"""
+    await bot.say('{} arguments: {}'.format(len(args), ', '.join(args)))
+
+@bot.command()
+async def kek():
+    await bot.say(lel)
+
+@bot.command()
+async def wagecuckalarm():
+    await bot.say("WEE WOO WEE WOO")
+
+@bot.command()
+async def profanities():
+    await bot.say()
+
+lel = u'\u314b\u314b\u314b'
+phrase = ["do your homework", 
+        "play cs", 
+        "take a hit",
+        "ruin someone's player experience"]
+img_dict = create_dict
+
+bot.run('INSERT TOKEN HERE')
