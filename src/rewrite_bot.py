721d1672b17959b6c09f871bfce0adf4347945ba
Kevin <kevinchau@utexas.edu>
2018-01-01 04:14:11 -0600

    Added favorites, working on alarm

--- src/rewrite_bot.py
+++ src/rewrite_bot.py
@@ -19,6 +19,11 @@ async def on_ready():
     print(bot.user.id)
     print("------")
 
+#async def alarm(self):
+#    await self.wait_until_read()
+#    channel = client.get_channel(326608470923149316)
+
+
 @bot.event
 async def on_message(message):
     channel = message.channel
@@ -29,7 +34,16 @@ async def on_message(message):
         if message.content == ("!8ballchau"):
             await channel.send(random.choice(phrase))
 
+    if len(message.mentions) > 0 and message.author.id != owen_id:
+        for member in message.mentions:
+            if owen_id == member.id or owenbot_id == member.id:
+                if message.author.id == cur_fav:
+                    await channel.send(heart_emoji)
+                else:
+                    await channel.send("fuck you")
+
     await bot.process_commands(message)
+
 
 @bot.command(description="Description of test")
 async def echo(ctx, *args):
@@ -51,14 +65,12 @@ async def kek(ctx):
 
 @bot.command()
 async def wagecuckalarm(ctx):
-    alarm_emoji = u'\u23F0'
     await ctx.send(alarm_emoji + "WEE WOO WEE WOO" + alarm_emoji)
     await ctx.send(alarm_emoji + "WAGE CUCKS GO TO SLEEP" + alarm_emoji)
     await ctx.send(alarm_emoji + "WEE WOO WEE WOO" + alarm_emoji)
 
 @bot.command()
 async def repost(ctx):
-    policecar_emoji = u'\U0001F693'
     await ctx.send(policecar_emoji + 
             "repost alert repost alert weewooweewooweewoo" +
             policecar_emoji)
@@ -72,6 +84,18 @@ async def addimg(ctx, cmd, img):
         f.write(cmd + " " + img)
     img_dict[cmd] = img
 
+@bot.command()
+async def favorite(ctx, mention):
+    if ctx.message.author.id == owen_id:
+        if len(ctx.message.mentions) > 1:
+            await ctx.send("fuck you only one favorite allowed")
+        else:
+            mention = int(mention[2:-1])
+            global cur_fav
+            cur_fav = mention
+    else:
+        await ctx.send("fuck you only owen gets to favorite")
+
 ### REMOVED COMMANDS ###
 
 lol = u'\u314b\u314b\u314b'
@@ -83,6 +107,19 @@ phrase = ["do your homework",
 cmd_list = [""]
 img_dict = create_dict()
 
+owen_id = 99359318305943552
+owenbot_id = 395883514056146944
+
+# Emojis
+heart_emoji = u'\u2764'
+alarm_emoji = u'\u23F0'
+policecar_emoji = u'\U0001F693'
+
+cur_fav = 0
+
 with open("token", "r") as f:
     token = f.readline()
+
 bot.run(token[:-1])
+#channel = client.get_channel(326608470923149316)
+#await channel.send('test')
