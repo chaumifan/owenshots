c6d3e8ee71659667b209647b868a35367fceb1f9
Kevin <kevinchau@utexas.edu>
2018-01-10 02:23:54 -0600

    Added spoiler command

--- src/rewrite_bot.py
+++ src/rewrite_bot.py
@@ -1,6 +1,11 @@
 import random
 import ngram
 import json
+import textwrap
+import os
+from PIL import Image, ImageDraw, ImageFont
+
+import discord
 from discord.ext import commands
 
 
@@ -110,12 +115,94 @@ async def mimic(ctx, word, num_words=0):
     sentence = ngram.ngram(counts, word, num_words)
     await ctx.send(sentence)
 
+@bot.command()
+async def spoiler(ctx, *, text: str):
+    # Source: 
+    # https://github.com/flapjax/FlapJack-Cogs/blob/master/spoiler/spoiler.py
+    temp_filepath = "spoiler/"
+    line_length = 60
+    margin = (5,5)
+    font = "spoiler/UbuntuMono-Regular.ttf"
+    font_size = 14
+    font_color = 150
+
+    message = ctx.message
+    author = message.author.display_name
+
+
+    title = "Mouseover to reveal spoiler"
+    if title == ''.join(text):
+        await ctx.send("Nice try :|")
+        return
+
+    try:
+        await message.delete()
+    except discord.errors.Forbidden:
+        await ctx.send("I require the 'manage messages' permission "
+                           "to hide spoilers!")
+
+    try:
+        fnt = ImageFont.truetype(font, font_size)
+    except OSError:
+        await ctx.send("I couldn't load the font file. Try "
+                           "reinstalling via the downloader cog, or "
+                           "manually place `UbuntuMono-Regular.ttf` "
+                           "in `/data/spoiler/`")
+        return
+
+    spoil_lines = []
+    for line in text.splitlines():
+        spoil_lines.extend(textwrap.wrap(line, line_length,
+                                         replace_whitespace=False))
+
+    width = fnt.getsize(title)[0] + 50
+    height = 0
+
+    for line in spoil_lines:
+        size = fnt.getsize(line)
+        width = max(width, size[0])
+        height += size[1] + 2
+
+    width += margin[0]*2
+    height += margin[1]*2
+
+    spoils = '\n'.join(spoil_lines)
+
+    spoil_img = [new_image(width, height) for _ in range(2)]
+    spoil_text = [title, spoils]
+
+    for img, txt in zip(spoil_img, spoil_text):
+        canvas = ImageDraw.Draw(img)
+        try:
+            canvas.text(margin, txt, font=fnt, fill=font_color,
+                        spacing=4)
+        except TypeError:
+            canvas.text(margin, txt, font=fnt, fill=font_color)
+
+    path = temp_filepath + ''.join(random.choice(
+               '0123456789ABCDEF') for i in range(12)) + ".gif"
+
+    spoil_img[0].save(path, format="GIF", save_all=True,
+                      append_images=[spoil_img[1]],
+                      duration=[0, 0xFFFF], loop=0)
+    content = "**" + author + "** posted this spoiler:"
+    
+    f = discord.File(path)
+    await ctx.send(file=f, content=content)
+    os.remove(path)
+
+def new_image(width, height):
+    return Image.new("L", (width, height), bg_color)
+
+
 ### REMOVED COMMANDS ###
 
 img_dict = create_dict()
 counts = ngram.ngram_init()
 
 cur_fav = 0
+
+bg_color = 20
 
 data = json.load(open("info.json"))
 token = data["token"]
